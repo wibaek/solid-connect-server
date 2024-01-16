af718a09826eab361861d9e4614114cffa66c68f
nayonsoso <panda0329@naver.com>
2024-01-16 21:28:14 +0900

    feat: 스프링 부트 초기 세팅

---
+++ src/main/java/com/example/solidconnection/SolidConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.solidconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidConnectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solidconnection/SolidConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solidconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
