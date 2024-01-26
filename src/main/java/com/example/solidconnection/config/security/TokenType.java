9eb115e93593f2d067df58089bdcf7e878bc1ace
nayonsoso <panda0329@naver.com>
2024-01-26 09:09:53 +0900

    feat: 스프링 시큐리티, JWT 토큰, Redis 관련 설정

---
+++ src/main/java/com/example/solidconnection/config/security/TokenType.java
@@ -0,0 +1,18 @@
+package com.example.solidconnection.config.security;
+
+import lombok.Getter;
+
+@Getter
+public enum TokenType {
+    ACCESS("", 1000 * 60 * 60),
+    REFRESH("refresh:", 1000 * 60 * 60 * 24 * 7),
+    KAKAO_OAUTH("kakao:", 1000 * 60 * 60);
+
+    private final String prefix;
+    private final int expireTime;
+
+    TokenType(String prefix, int expireTime){
+        this.prefix = prefix;
+        this.expireTime = expireTime;
+    }
+}
