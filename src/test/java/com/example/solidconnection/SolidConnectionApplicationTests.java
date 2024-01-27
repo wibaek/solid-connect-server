02a5dd2e1a176b5473ae071d96fcac8f125c1f54
nayonsoso <panda0329@naver.com>
2024-01-27 18:55:54 +0900

    fix: 테스트 실패 에러 해결

--- src/test/java/com/example/solidconnection/SolidConnectionApplicationTests.java
+++ src/test/java/com/example/solidconnection/SolidConnectionApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.solidconnection;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SolidConnectionApplicationTests.class)
 class SolidConnectionApplicationTests {
 
     @Test
