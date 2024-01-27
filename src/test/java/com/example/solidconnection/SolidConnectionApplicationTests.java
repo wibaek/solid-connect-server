f336a6257eec1c2b12c9ee1251721f61011990ba
nayonsoso <panda0329@naver.com>
2024-01-27 19:18:18 +0900

    test: 테스트 코드 수정

--- src/test/java/com/example/solidconnection/SolidConnectionApplicationTests.java
+++ src/test/java/com/example/solidconnection/SolidConnectionApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.solidconnection;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SolidConnectionApplicationTests.class)
+@SpringBootTest
 class SolidConnectionApplicationTests {
 
     @Test
