b8bd0cb30a94dcbd7afdbf94211452cb6ccaba16
Yeongseo Na <panda0329@naver.com>
2024-02-20 19:56:02 +0900

    refactor: cors 허용 origin 추가

--- src/main/java/com/example/solidconnection/config/cors/WebConfig.java
+++ src/main/java/com/example/solidconnection/config/cors/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.solid-connect.net")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
