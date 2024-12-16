c0426c93df476197103ebc0cf4280516c8adba18
Wibaek Park <34394229+wibaek@users.noreply.github.com>
2024-12-16 12:52:02 +0900

    chore: 호스팅 도메인 변경과 nginx 제거에 따른 전환 (#118)
    * chore: nginx docker-compose에서 제거
    
    * chore: cross-origin 허용 url 전환
    
    * chore: nginx 제거와 docker-compose 명령 변경에 따른 workflow 수정
    
    * feat: CorsPropertiesConfig 추가, cors allowed origins 정보를 yml로 분리한다
    
    * style: 주석 삭제

--- src/main/java/com/example/solidconnection/config/cors/WebConfig.java
+++ src/main/java/com/example/solidconnection/config/cors/WebConfig.java
@@ -1,16 +1,20 @@
 package com.example.solidconnection.config.cors;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final CorsPropertiesConfig corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.solid-connect.net")
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
